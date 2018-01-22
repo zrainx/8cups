039498080d0a0a71f96e0532549b6b7107f1ca6c
zrainx <zrainx@foxmail.com>
2018-01-23 00:14:28 +0800

    Update sketch_fsr420.ino

--- sketch_fsr420.ino
+++ sketch_fsr420.ino
@@ -18,18 +18,5 @@ void loop(void) {
   String ss=String("Adc: ")+fsrReading;
   
   u8x8.drawString(0,0, ss.c_str());
-/*  
-  if (fsrReading < 10) {
-    Serial.println(" - No pressure");
-  } else if (fsrReading < 200) {
-    Serial.println(" - Light touch");
-  } else if (fsrReading < 500) {
-    Serial.println(" - Light squeeze");
-  } else if (fsrReading < 800) {
-    Serial.println(" - Medium squeeze");
-  } else {
-    Serial.println(" - Big squeeze");
-  }
-*/
   delay(1000);
 }
